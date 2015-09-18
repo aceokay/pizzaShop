cff604ce9eaa70231365b278a4f397a87f130aad
Austin <aokincaid@gmail.com>
2015-09-18 10:09:38 -0700

    Add spec and method to biuld new size instances for pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 function Topping(name) {
   this.name = name;
 }
+
+function Size() {
+  this.small = false;
+  this.medium = false;
+  this. large = false;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -4,3 +4,12 @@ describe("Topping", function() {
     expect(newTopping.name).to.equal("Whole Pepper");
   });
 });
+
+describe("Size", function() {
+  it("creates a new instance of Size object with size options set to false", function() {
+    var newSize = new Size();
+    expect(newSize.small).to.equal(false);
+    expect(newSize.medium).to.equal(false);
+    expect(newSize.large).to.equal(false);
+  });
+});
